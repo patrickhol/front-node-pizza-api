55ab897d602f9e5c2ad4f272b8a2512c0331af7f
Patrick H <patryk.holody@gmail.com>
2018-12-06 14:52:47 +0100

    Add Cluster

---
+++ index-one-thread.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * Primary file for the API pizza-delivery company. One Thread
+ * 
+ */
+
+// Dependencies
+const server = require('./lib/server');
+const cli = require('./lib/cli');
+
+
+const app = {};
+
+// Init function
+app.init = () => {
+
+    // Start the server
+    server.init();
+
+    // Start cli last important
+    setTimeout(() => {
+        cli.init();
+    }, 50);
+
+};
+
+
+app.init();
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 /**
  * 
- * Primary file for the API pizza-delivery company.
+ * Primary file for the API pizza-delivery company. Cluster active
  * 
  */
 
 // Dependencies
 const server = require('./lib/server');
 const cli = require('./lib/cli');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
 
 const app = {};
 
 // Init function
 app.init = () => {
-    // Start the server
-    server.init();
+    if (cluster.isMaster) {
 
-    // Start cli last important
-    setTimeout(() => {
-        cli.init();
-    }, 50);
+
+        // Start cli last important
+        setTimeout(() => {
+            cli.init();
+
+        }, 50);
+        console.log(`Threet Master ${process.pid} is running`);
+
+        // Fork workers.
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`Threet Master ${worker.process.pid} died`);
+        });
+
+    } else {
+        // Start the server
+        server.init();
+        console.log(`Threet child ${process.pid} started`);
+    }
 
 };
 
